2721eb757dc132c073f190a9663e6fd1aa548648
GowthamR777 <thamgow277@gmail.com>
2026-05-08 14:23:30 +0530

    customer_segmentation.py

--- Customer-Segmentation-ML/customer_segmentation.py
+++ Customer-Segmentation-ML/customer_segmentation.py
@@ -1,1 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
+# Load dataset
+data = pd.read_csv("Mall_Customers.csv")
+
+# Select features
+X = data[['Annual Income (k$)', 'Spending Score (1-100)']]
+
+# Elbow Method
+wcss = []
+
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+# Plot Elbow Graph
+plt.plot(range(1, 11), wcss, marker='o')
+plt.title("Elbow Method")
+plt.xlabel("Number of Clusters")
+plt.ylabel("WCSS")
+plt.show()
+
+# Train KMeans
+kmeans = KMeans(n_clusters=5, random_state=42)
+
+# Predict clusters
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualize Clusters
+plt.scatter(
+    X.iloc[:, 0],
+    X.iloc[:, 1],
+    c=y_kmeans,
+    cmap='rainbow'
+)
+
+# Centroids
+plt.scatter(
+    kmeans.cluster_centers_[:, 0],
+    kmeans.cluster_centers_[:, 1],
+    s=200,
+    c='black',
+    label='Centroids'
+)
+
+plt.title("Customer Segmentation")
+plt.xlabel("Annual Income")
+plt.ylabel("Spending Score")
+plt.legend()
+plt.show()
